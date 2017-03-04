9f1be1fc86443382166f67a154c885d2f7dbf868
Emilio Garcia <Emilio Garcia>
2017-03-04 16:38:02 -0500

    web server -- basic

---
+++ server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+
+        # Send message back to client
+        message = "Hello world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
+def run():
+    print("starting up server...")
+
+    server_address = ('127.0.0.1', 8081)
+    httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+    print('running server...')
+    httpd.serve_forever()
+
+run()
